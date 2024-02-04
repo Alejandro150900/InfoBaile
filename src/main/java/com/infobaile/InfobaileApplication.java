1d6177e0cb3a20910890340b04dd5fe9bfad3429
Alejandro150900 <luisaa00-@hotmail.com>
2024-02-03 18:19:02 -0600

    Primer commit (archivos default)

---
+++ src/main/java/com/infobaile/InfobaileApplication.java
@@ -0,0 +1,13 @@
+package com.infobaile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfobaileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfobaileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infobaile/InfobaileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infobaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfobaileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
